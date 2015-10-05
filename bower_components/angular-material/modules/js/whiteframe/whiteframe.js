356a96cac7eae3cca1916f793900f263e78562a5
Elrashid <osman.elrashid@gmail.com>
2015-10-05 19:18:16 +0400

    add bower_components

---
+++ bower_components/angular-material/modules/js/whiteframe/whiteframe.js
@@ -0,0 +1,16 @@
+/*!
+ * Angular Material Design
+ * https://github.com/angular/material
+ * @license MIT
+ * v0.11.2-master-71674b0
+ */
+(function( window, angular, undefined ){
+"use strict";
+
+/**
+ * @ngdoc module
+ * @name material.components.whiteframe
+ */
+angular.module('material.components.whiteframe', []);
+
+})(window, window.angular);
